f5f5749b98cc609bc4d97cf3fb4d04760c9f6ec4
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-10-08 14:06:20 +0800

    change equipment to equipmentName

--- migrations/20211008051247-create-equipment.js
+++ migrations/20211008051247-create-equipment.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      equipment: {
+      equipmentName: {
         type: Sequelize.STRING
       },
       createdAt: {
