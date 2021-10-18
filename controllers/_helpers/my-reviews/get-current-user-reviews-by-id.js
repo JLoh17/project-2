2c2900c4172ff2dde83db72764639a859bddd3a5
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-10-18 11:16:41 +0800

    my reviews done

--- controllers/_helpers/my-reviews/get-current-user-reviews-by-id.js
+++ controllers/_helpers/my-reviews/get-current-user-reviews-by-id.js
@@ -1,18 +1,40 @@
-const { Rating } = require('../../../models')
+const { Rating, Equipment } = require('../../../models')
 
-module.exports = function(format) {
+module.exports = function(format, useAssociation) {
   return async function (req, res, next) {
     const { locals: { currentUser } } = res
     const { params: { id } } = req
+
+    let options = {}
+
+    if (useAssociation) {
+      options = {
+        include: [
+          {
+            required: true,
+            association: Rating.Equipment,
+            duplicating: false,
+            include: {
+              association: Equipment.Comments,
+              required: false, // false otherwise will duplicate extra equipment where comment = 0. Basically need to do this for all
+              duplicating: false,
+              where: {
+                UserId: currentUser.id
+              }
+            }
+          }, {
+            association: Rating.User
+          }
+        ]
+      }
+    }
+
     const rating = await Rating.findOne({
       where: {
         id: Number(id) || 0,
         UserId: currentUser.id
       },
-      include: {
-        association: Rating.Equipment
-      },
-      order: [['EquipmentId', 'DESC']]
+      ...options
     })
 
     if (!rating) {
--- controllers/api/my-reviews/show.js
+++ controllers/api/my-reviews/show.js
@@ -3,12 +3,12 @@ const { authenticateCurrentUserByToken ,
 } = require('../../_helpers')
 
 const apiMyReviewsShow = async function(req, res) {
-  const { locals: { review } } = res
-  res.render('api/my-reviews/show', { review, layout: false })
+  const { locals: { currentReview } } = res
+  res.render('api/my-reviews/show', { Rating: currentReview, layout: false })
 }
 
 module.exports = [
   authenticateCurrentUserByToken('json'),
-  getCurrentUserReviewsById('modal'),
+  getCurrentUserReviewsById('modal', true),
   apiMyReviewsShow,
 ]
